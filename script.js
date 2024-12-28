e86bcaebaddefbb0b1072fd766f9963e2aeea01a
firefly221 <bbudyn01@gmail.com>
2024-12-28 18:03:50 +0100

    Added gui

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
+const container = document.querySelector("#container");
+
+
+
+
+const rock = document.createElement("button");
+const paper = document.createElement("button");
+const scissors = document.createElement("button");
+
+rock.textContent = "Rock";
+paper.textContent = "Paper";
+scissors.textContent = "Scissors";
+rock.id = 0;
+paper.id = 1;
+scissors.id = 2;
+
+container.appendChild(rock);
+container.appendChild(paper);
+container.appendChild(scissors);
+
+
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.style.margin = "20px";
+});
+
+
+
+const scoreField = document.createElement("h1");
+scoreField.textContent = "0";
 
 
 
 
 
+buttons.forEach((button) => {
+    button.addEventListener("click",()=>{
+        const choice = button.id;
+        alert(choice);
+        
 
+    });
+});
 
 
 
